6aa41c0b5d46bd3388a22937302a2364fc6d66d0
Nevsen <alexnevsen@gmail.com>
2020-11-09 22:11:17 +0300

    relocate**

---
+++ templates/layouts/calc.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Calculator</title>
+</head>
+<body>
+	<header>
+		<?= $menu ?>
+	</header>
+	<div class="container">
+		<h1>Калькулятор</h1>
+		<?= $content ?>
+	</div>
+</body>
+</html>
